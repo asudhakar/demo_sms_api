af5f088228809da904b138bc5965ce933f6dd830
sudhakar <asudhakar@live.in>
2016-06-13 14:59:11 +0530

    delete file added

--- view/insert_into_db.php
+++ view/insert_into_db.php
@@ -6,11 +6,6 @@ $name_and_numbers = $_POST['name_and_number'];
 $message = $_POST['message'];
 $name_and_numbers = unserialize(base64_decode($name_and_numbers));
 $path = $_POST['path'];
-
-
-
-
-
 $link = db_connect();
 
 foreach ($name_and_numbers as $key => $number) {
