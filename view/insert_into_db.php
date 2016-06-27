170194c946b505f47de89805fd258b03880dcd7b
sudhakar <asudhakar@live.in>
2016-06-27 11:41:34 +0530

    $name work

--- view/insert_into_db.php
+++ view/insert_into_db.php
@@ -8,13 +8,16 @@ $name_and_numbers = unserialize(base64_decode($name_and_numbers));
 $path = $_POST['path'];
 $link = db_connect();
 
-foreach ($name_and_numbers as $key => $number) {
-	$sql = "INSERT INTO `MessageOut` (`MessageTo` , `MessageText`) VALUES ('$number', '$message')";
-	mysqli_set_charset($link, 'utf8mb4'); 
-	executeQuery($sql, $link);
+
+print_r(strpos($message, '$name'));
+
+// foreach ($name_and_numbers as $key => $number) {
+// 	$sql = "INSERT INTO `MessageOut` (`MessageTo` , `MessageText`) VALUES ('$number', '$message')";
+// 	mysqli_set_charset($link, 'utf8mb4'); 
+// 	executeQuery($sql, $link);
 	
-}
-header('Location: ../index.php');
+// }
+// header('Location: ../index.php');
 
 	function db_connect(){
 		$connection = mysqli_connect('103.207.0.76', "demo", "demo1234", "demo", 3306);
