2d99554e0a6c8f652451f6759d2d2f88b4c408be
sudhakar <asudhakar@live.in>
2016-07-24 22:38:21 +0530

    testing

--- view/mark_upload_process.php
+++ view/mark_upload_process.php
@@ -1,3 +1,30 @@
 <?php 
 
-print_r($_FILES);
+	$target_dir = "../files/";
+	$target_file = $target_dir . basename($_FILES["markUpload"]["name"]);
+	$uploadOk = 1;
+	if (file_exists($target_file)) {
+	    $uploadOk = 0;
+	}
+
+	if ($_FILES["markUpload"]["size"] > 500000) {
+	    $uploadOk = 0;
+	}
+
+	if ($uploadOk == 0) {
+	} else {
+	    if (move_uploaded_file($_FILES["markUpload"]["tmp_name"], $target_file)) {
+	    	default_message_process($target_file);
+	    } else {
+	    	echo "Error in inserting";
+	    }
+	}
+
+
+	function default_message_process($target_file){
+		require('../php-excel-reader/excel_reader2.php');
+		require('../SpreadsheetReader.php');
+		echo "$target_file<br/><pre>";
+		$Reader = new SpreadsheetReader($target_file);
+		print_r($Reader);
+	}
