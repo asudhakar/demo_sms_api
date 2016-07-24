bbc10d2e93035a224d759047d9395bf6439abc98
sudhakar <asudhakar@live.in>
2016-07-24 22:23:17 +0530

    bug fixed

--- view/mark_upload_process.php
+++ view/mark_upload_process.php
@@ -1,19 +1,19 @@
 <?php 
 include_once '../controllers/function.php';
 $target_dir = "../temp/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$target_file = $target_dir . basename($_FILES["markUpload"]["name"]);
 $uploadOk = 1;
 if (file_exists($target_file)) {
     $uploadOk = 0;
 }
 echo $target_file;
-if ($_FILES["fileToUpload"]["size"] > 500000) {
+if ($_FILES["markUpload"]["size"] > 500000) {
     $uploadOk = 0;
 }
 
 if ($uploadOk == 0) {
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    if (move_uploaded_file($_FILES["markUpload"]["tmp_name"], $target_file)) {
     	echo "file saved";
     } else {
     	echo "Error in inserting";
