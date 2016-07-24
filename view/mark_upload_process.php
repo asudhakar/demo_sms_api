94c8ae5400f7880fe10610ac52c217ae5f34c597
sudhakar <asudhakar@live.in>
2016-07-24 22:33:01 +0530

    testing

--- view/mark_upload_process.php
+++ view/mark_upload_process.php
@@ -1,28 +1,3 @@
 <?php 
-include_once '../controllers/function.php';
-require('../php-excel-reader/excel_reader2.php');
-require('../SpreadsheetReader.php');
-$target_dir = "../temp/";
-$target_file = $target_dir . basename($_FILES["markUpload"]["name"]);
-$uploadOk = 1;
-if (file_exists($target_file)) {
-    $uploadOk = 0;
-}
-echo $target_file;
-if ($_FILES["markUpload"]["size"] > 500000) {
-    $uploadOk = 0;
-}
 
-if ($uploadOk == 0) {
-} else {
-    if (move_uploaded_file($_FILES["markUpload"]["tmp_name"], $target_file)) {
-    	echo "file saved";
-    } else {
-    	echo "Error in inserting";
-    }
-}
-
-
-$Reader = new SpreadsheetReader($target_file);
-
-print_r($Reader);
+print_r($_FILES);
