ad8aa030f39218cde8164ff9f07ce8ca808f39f2
sudhakar <asudhakar@live.in>
2016-07-24 23:09:37 +0530

    test

--- view/mark_upload_process.php
+++ view/mark_upload_process.php
@@ -1,4 +1,5 @@
 <?php 
+	include_once '../controllers/function.php';
 	print_r($_FILES);
 	$target_dir = "../temp/";
 	$target_file = $target_dir . basename($_FILES["markUpload"]["name"]);
@@ -15,6 +16,7 @@
 	} else {
 	    if (move_uploaded_file($_FILES["markUpload"]["tmp_name"], $target_file)) {
 	    	default_message_process($target_file);
+	    	delete_target_file($target_file);
 	    } else {
 	    	echo "Error in inserting";
 	    }
@@ -22,6 +24,7 @@
 
 
 	function default_message_process($target_file){
+		$link = db_connect();
 		echo "$target_file";
 		require('../php-excel-reader/excel_reader2.php');
 		require('../SpreadsheetReader.php');
@@ -33,7 +36,31 @@
 				$i++;
 				continue;
 			} else{
-				print_r($Row);
+				$message_content = "Your Son/Daughter ".$Row['1']."Scored Tam-I :- ".$Row['2'].", Tam-II :-".$Row['3']."Tamil Total :-".$Row['4']."Eng-I :-".$Row['5']."Eng-II :-".$Row['6']."Total English :-".$Row['7']."Phy :-".$Row['8']."Che :-".$Row['9']."Bio/CS :-".$Row['10']."Mat :-".$Row['11']."Total :-".$Row['12']."Rank of your Son/Daughter is".$Row['13'];
+				$sql = "INSERT INTO `MessageOut` (`MessageTo` , `MessageText`) VALUES ('$Row[14]', '$message_content')";
+				echo "$sql";
+				// mysqli_set_charset($link, 'utf8mb4'); 
+				// executeQuery($sql, $link);
 			}
+		}
+	}
+
+	function db_connect(){
+		$connection = mysqli_connect('103.207.0.76', "demo", "demo1234", "demo", 3306);
+		if (!$connection) {
+		    die("Connection failed: " . mysqli_connect_error());
+		    exit();
+		}
+		return $connection;
+	}
+
+	function delete_target_file($path){
+		if (!unlink($path))
+		{
+		  echo ("Error deleting $path");
+		}
+		else
+		{
+		  echo ("Deleted $path");
 		}
 	}
